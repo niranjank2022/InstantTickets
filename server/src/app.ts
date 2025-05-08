469e32818f6e2f47510980c9a0012260d73dccae
Niranjan <niranjank.desktop@gmail.com>
2025-05-09 02:37:18 +0530

    add client origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,6 @@ import { messages } from './config/logger';
 
 // Create the express app
 const app: Application = express();
-
 const allowedOrigins = ['http://localhost:5173', 'https://instanttickets.onrender.com'];
 
 // Add middlewares before routes
